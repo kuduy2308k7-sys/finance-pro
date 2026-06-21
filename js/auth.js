e19d17dd8101c75098d76ea6c5cddca7c31355f1
kuduy2308k7-sys <kuduy2308k7@gmail.com>
2026-06-21 23:57:00 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -7,31 +7,18 @@ import {
 }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-export async function register(email,password){
-
-    return await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-    );
+export async function register(email, password){
+    return await createUserWithEmailAndPassword(auth, email, password);
 }
 
-export async function login(email,password){
-
-    return await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-    );
+export async function login(email, password){
+    return await signInWithEmailAndPassword(auth, email, password);
 }
 
 export async function logout(){
-
     return await signOut(auth);
 }
 
-// Đoạn code test tạm thời cho Bước 6
-import { auth } from "./firebase.js";
-
+// === ĐOẠN CODE TEST BƯỚC 6 (ĐÃ XÓA DÒNG IMPORT TRÙNG) ===
 console.log("--- KIỂM TRA KẾT NỐI FIREBASE ---");
 console.log(auth);
